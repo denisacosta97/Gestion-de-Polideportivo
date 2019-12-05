d17d48570565727b525c7aff732634fff028c7b1
Denis Acosta <denisacosta97@hotmail.com>
2019-12-05 13:59:29 -0300

    Initial commit

---
+++ app/src/main/java/com/unse/gestiondepolideportivo/SplashScreenActivity.java
@@ -0,0 +1,74 @@
+package com.unse.gestiondepolideportivo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ProgressBar mProgress;
+    Animation mAnimation;
+    LinearLayout linearFondo;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setViewsConfig();
+
+        loadViews();
+
+        loadData();
+
+        loadListener();
+
+    }
+
+    private void loadListener() {
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 4500);
+    }
+
+    private void loadData() {
+        Glide.with(this).load(R.color.colorPrimary)
+                .into(linearFondo);
+
+    }
+
+    private void loadViews() {
+        mProgress = findViewById(R.id.splash_screen_progress_bar);
+        linearFondo = findViewById(R.id.splashbackgr);
+
+    }
+
+    private void setViewsConfig() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+
+}
